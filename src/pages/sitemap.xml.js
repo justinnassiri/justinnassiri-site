e09e4555198c19fe19a81c86213aa58c12f229de
justinnassiri <justin@executivepresence.io>
2026-06-23 18:45:14 -0600

    Update sitemap.xml.js

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -3,7 +3,7 @@ import { SITE } from '../consts';
 export async function GET() {
   const writing = await getCollection('writing');
   const podcast = await getCollection('podcast');
-  const paths = ['', 'about', 'writing', 'podcast',
+  const paths = ['', 'about', 'writing', 'podcast', 'appearances', 'watch',
     ...writing.map((p) => `writing/${p.slug}`),
     ...podcast.map((e) => `podcast/${e.slug}`)];
   const urls = paths.map((p) => `  <url><loc>${SITE.url}/${p}${p ? '/' : ''}</loc></url>`).join('\n');
